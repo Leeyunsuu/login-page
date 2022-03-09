fdec27bce6dd6089403c416f7f83d6d6f11527c2
DEENNO <yuntteng31@gmail.com>
2022-03-09 17:28:22 +0900

    기능::POST API생성 후 fetch데이터가 이상없이 controller에 전달 되는지 테스트

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 //모듈
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 //라우팅
@@ -11,6 +12,11 @@ const home = require('./src/routes/home');
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/src/public`));
+//URL을 통해 전달되는 한글, 공백 등의 문자가 포함된 경우, 인식되지 않는 문제 해결
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', home); //use => Middleware
 
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -4,7 +4,7 @@ const id = document.querySelector('#id');
 const psword = document.querySelector('#psword');
 const loginBtn = document.querySelector('button');
 
-loginBtn.addEventListener('click', login);
+loginBtn.addEventListener('click', Login);
 
 function Login() {
   const req = {
