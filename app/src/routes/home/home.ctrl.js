04896c778862af5baae5c840f2cc412443fa3040
DEENNO <yuntteng31@gmail.com>
2022-03-09 18:17:13 +0900

    기능:: 로그인 기능 구현(backend)

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,9 +9,29 @@ const output = {
   },
 };
 
+const users = {
+  id: ['yunsuteng', 'eunjii', 'jang'],
+  psword: ['0000', '1111', '2222'],
+};
+
 const process = {
   login: (req, res) => {
-    console.log('home.crtl.js : ' + JSON.stringify(req.body, 2, null));
+    const id = req.body.id;
+    const psword = req.body.psword;
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id); //해당 id의 배열
+      if (users.psword[idx] === psword) {
+        //만약 해당 psword배열과 frontend에서 입력한 value가 일치할때
+        return res.json({
+          success: true,
+          msg: '로그인 성공...',
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: '로그인 실패...',
+    });
   },
 };
 
