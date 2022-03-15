39cd89499beca91629c9ed706a35989b4d319009
DEENNO <yuntteng31@gmail.com>
2022-03-15 17:18:27 +0900

    기능::회원가입 요청 구현(Frontend)

---
+++ app/src/public/js/home/finder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const id = document.querySelector('#id');
+const psword = document.querySelector('#psword');
+const finderBtn = document.querySelector('#finder');
+console.log('finder');
+
+finderBtn.addEventListener('click', finder);
+
+function finder() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+
+  fetch('/finder', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        //로그인 성공 시
+        location.href = '/';
+      } else {
+        alert(res.msg);
+        // console.log(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error('로그인 에러'));
+    });
+}
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,19 +1,25 @@
 'use strict';
 
+//login
 const id = document.querySelector('#id');
 const psword = document.querySelector('#psword');
 const loginBtn = document.querySelector('#login');
 
+//sign up
+const signupId = document.querySelector('#signupId');
+const signupPsword = document.querySelector('#signupPsword');
+const confirmPsword = document.querySelector('#confirm-psword');
+const emailAdress = document.querySelector('#email');
+const signupBtn = document.querySelector('#signup');
+
 loginBtn.addEventListener('click', Login);
+signupBtn.addEventListener('click', Register);
 
 function Login() {
   const req = {
     id: id.value,
     psword: psword.value,
   };
-  console.log(req);
-  console.log(JSON.stringify(req, 2, null));
-  console.log('login 중 ...');
 
   fetch('/login', {
     method: 'POST',
@@ -25,6 +31,35 @@ function Login() {
       if (res.success) {
         //로그인 성공 시
         location.href = '/';
+      } else {
+        alert(res.msg);
+        // console.log(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error('로그인 에러'));
+    });
+}
+
+function Register() {
+  const req = {
+    id: signupId.value,
+    psword: signupPsword.value,
+    confirmPsowrd: confirmPsword.value,
+    emailAdress: emailAdress.value,
+  };
+
+  fetch('/register', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        //로그인 성공 시
+        alert(res.msg);
+        location.href = '/login';
       } else {
         alert(res.msg);
         // console.log(res.msg);
